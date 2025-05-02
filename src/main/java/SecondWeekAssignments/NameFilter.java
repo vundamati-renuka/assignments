b091a43aae90d8e051168d5acdd7110933917c2e
vundamati-renuka <vundamatirenuka@gmail.com>
2025-05-02 11:41:39 +0530

    Update NameFilter.java

--- src/main/java/SecondWeekAssignments/NameFilter.java
+++ src/main/java/SecondWeekAssignments/NameFilter.java
@@ -1,4 +1,27 @@
 package SecondWeekAssignments;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class NameFilter {
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+        names.add("Anand");
+        names.add("Bob");
+        names.add("Annie");
+        names.add("Charlie");
+        names.add("Amos");
+
+
+        Collections.sort(names);
+
+        System.out.println("Names starting with 'A':");
+        for (String name : names) {
+            if (name.startsWith("A")) {
+                System.out.println(name);
+            }
+        }
+    }
 }
